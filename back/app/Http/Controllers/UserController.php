4d2c3c3c24eb9971198056269c110d2d85e16a46
vunyin <vun.yin@student.passerellesnumeriques.org>
2021-11-26 12:51:44 +0700

    signin-signup api

---
+++ back/app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function signup(Request $request){
+        $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+        //create user
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+
+        $user->save();
+
+        //create Token
+
+        $token = $user->createToken('mytoken')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token'=> $token,
+        ]);
+    }
+    public function signin(Request $request){
+        
+        // check email
+        $user = User::where('email',$request->email)->first();
+
+        // check password
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Bad login'],401);
+        }
+
+        //create Token
+        $token = $user->createToken('mytoken')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token'=> $token,
+        ]);
+    }
+
+}
+
+// "1|kkS0kI5nBzKFg12lDvAfRclXMMXLcWVUnfQimjsz"
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//public
+Route::post('/signup', [UserController::class, 'signup']);//register, create new account
+
+//private Route
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/signin', [UserController::class, 'signin']);
 });
