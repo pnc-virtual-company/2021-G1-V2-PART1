bb01ccc0d5983b160b3ad4d8ff200ebdf5431d9c
sophannamey <sophanna.mey@student.passerellesnumeriques.org>
2021-11-27 11:27:34 +0700

    initialize project

--- front/src/main.js
+++ front/src/main.js
@@ -1,19 +1,6 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-import Themenu from './components/navigation/Themenu.vue'
 
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: Themenu },
-    // { path: '/students', component: StudentView },
-    // { path: '/students/:studentId', component: StudentDetails, props: true },
-    // { path: '/:notFound(.*)', component: StudentNotFound },
-  ],
-});
 
 const app = createApp(App);
-app.use(router);
 app.mount("#app");
