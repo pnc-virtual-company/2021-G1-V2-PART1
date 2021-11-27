a771f52c576ce05a90086d366869c9e31062a1d9
sophannamey <sophanna.mey@student.passerellesnumeriques.org>
2021-11-27 11:24:53 +0700

    #8 add manu bar

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import Themenu from './components/navigation/Themenu.vue'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Themenu },
+    // { path: '/students', component: StudentView },
+    // { path: '/students/:studentId', component: StudentDetails, props: true },
+    // { path: '/:notFound(.*)', component: StudentNotFound },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
