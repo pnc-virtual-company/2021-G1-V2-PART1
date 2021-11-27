5f8743d02419a3abb9d5003bce2355191c034699
CHANTHY-THA <chanthy.tha@student.passerellesnumeriques.org>
2021-11-28 00:22:27 +0700

    add app.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -13,12 +13,6 @@ export default {
   },
   data() {
     return {
-      activePage: 'teams-list',
-      teams: [
-        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
-        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
-        { id: 't3', name: 'Dream Team', members: ['u4', 'u1'] },
-      ],
       users: [
         { id: 'u1', fullName: 'Ronan OGOR', role: 'The king' },
         { id: 'u2', fullName: 'Him Hey', role: 'The Queen' },
