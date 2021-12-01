e7d04655981a5b5c54a827ebc464bb70a98b2ae4
CHANTHY-THA <chanthy.tha@student.passerellesnumeriques.org>
2021-12-01 23:20:36 +0700

    add categories view and create

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,35 +1,17 @@
 <template>
   <section>
-    <router-view @add-category="addCategory"></router-view>
-    
-    <!-- <categories-view
-      v-for="category of categoriesList" 
-      :key='category.id'
-      :category="category" 
-    ></categories-view> -->
-    
+    <router-view></router-view>
   </section>
 </template>
 
 
 <script>
-// import CategoriesView from './components/categories/categories-view.vue'
-// import signup from './components/Signin-signup/signup.vue'
 
 export default {
   data() {
     return{
       userLists:[],
       categoriesList: [],
-    }
-  },
-  methods: {
-    addCategory(title){ 
-      const newCategory = {
-        title: title,
-      }
-      this.categoriesList.push(newCategory)
-      console.log(this.categoriesList);
     }
   },
 }
--- front/src/components/categories/categories-view.vue
+++ front/src/components/categories/categories-view.vue
@@ -1,99 +1,91 @@
 <template>
     <section>
-        <div class="cateAndBtn">
-            <h3>Category</h3>
-            <div class="btnCreate">
-                <button @click="createCategory" class="CtCreate">Create</button>
+        <div class="form">
+            <button type="button" id="createBtn" class="btn btn-success" data-toggle="modal" data-target="#exampleModalCenter">+Create categories</button>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Categories title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <input type="text" placeholder="categories name" v-model="categoryName">
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button @click="createCategory" type="button" class="btn btn-primary" data-dismiss="modal">Create</button>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
-        <div class="search">
-        <input type="search" name="search" id="" placeholder="Search...">
-    </div>
-    <div class="category">
-      
-        <div class="veiwCard">
+        
+        <!-- //================search btn==================== -->
+
+        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+
+       <!-- ============categories========================== -->
+
+        <div class="category">
             <p class="card-title">Sport</p>
-            <!-- <div class="btn">
-                <button class="delete"><i class="fa fa-remove" style="font-size:24px"></i></button>
-                <button class=""><i class="fa fa-pencil" style="font-size:24px"></i></button>
-            </div> -->
+            <p class="card-title">Music</p>
+            <p class="card-title">paino</p>
+            <p class="card-title">Game</p>
         </div>
-    </div>
+
     </section>
-    
 </template>
 <script>
 export default {
-    props:['category'],
     data() {
         return{
-            test: ""
+            categoryName: ""
         }
     },
     methods: {
         createCategory() {
-            this.$router.push('/categoriesForm');
-            console.log(this.category);
+            console.log(this.categoryName);
+            this.categoryName = "";
         }
     }
 }
 </script>
 
 <style >
-    .cateAndBtn{
+    #createBtn{
+        margin-left: 60%;
         margin-top: 5%;
-        margin-bottom: -6%;
-        display: flex;
-        justify-content: space-between;
+        margin-bottom: 3%;
+    }
+    .form-control{
         width: 40%;
         margin-left: 32%;
     }
-    .CtCreate{
-        padding:8px 10px;
-        background-color: rgb(3, 55, 133);
+   
+    .category{
+        width: 40%;
+        background: rgba(122, 185, 161, 0.596);
+        margin-left: 32%;
+        margin-top: 3%;
         border-radius: 5px;
-        outline:none;
-        color: white;
-        cursor: pointer;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     }
-    .cateAndBtn h3{
-        text-shadow: 1px 1px #302e2e;
+    .card-title{
+        padding: 10px ;
     }
-    .search{
-    margin-top: 100px;
-    margin-bottom: -100px;
-}
-input[type=search]{
-    width: 40%;
-    height: 5vh;
-    border-radius: none;
-    margin-left: 32%;
-    border-radius: 5px;
-    font-size: 17px;
-    padding-left: 10px;
-}
+    .card-title:hover{
+        background: rgba(72, 147, 190, 0.527);
+        border-radius: 5px;
+    }
+    input[type=text] {
+        border: 1px solid rgb(82, 79, 79);
+        outline: none;
+    }
+    h2{
+        
+    }
 
-
-.category{
-    width: 40%;
-    background-color: rgb(134, 139, 139);
-    margin-left: 32%;
-    margin-top: 10%;
-    border-radius: 10px;
-}
-.veiwCard{
-    display: flex;
-    margin-top: 0;
-    margin: 0;
-    padding: 1px;
-    background: rgb(124, 133, 133);
-    border-radius: 10px;
-}
-.veiwCard:hover{
-    background-color: rgb(110, 103, 103);
-    cursor: pointer;
-}
-h3{
-    margin-left: 0px;
-}
 </style>
