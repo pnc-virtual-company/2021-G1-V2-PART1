9e509547f36571af723e13112082f839c495c726
sreytouchmouch <sreytouch.mouch@student.passerellesnumeriques.org>
2021-12-09 10:17:47 +0700

    #50 update search event

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,6 +7,7 @@
     </div>
 
     <div class="event" v-if = "findEvent">
+      <input v-on:keyup = "search" type="search" placeholder="search event to join..." v-model= "searchevent">
       <ul>
         <findEvent-card
           v-for="event of eventLists"
@@ -15,8 +16,6 @@
         ></findEvent-card>
       </ul>
     </div>
-   
-
   </section>
 </template>
 
@@ -38,20 +37,37 @@ export default {
       isFindevent: true,
       findEvent: false,
       eventLists: [],
+      eventName: "",
+      searchevent:"",
+      eventInfo: "",
     }
   },
   methods: {
     Findevent(findevent){
       this.isFindevent = findevent;
       this.findEvent = !findevent ;
-      console.log(this.isFindevent + " " + this.findEvent);
+      this.eventInfo = findevent;
+      console.log(this.isFindevent + " " + this.findEvent + "" + this.eventInfo);
     },
     
     getEvent(){
       axios.get(API_URL).then(res => {
         this.eventLists = res.data;
+        this.eventName = "";
+        this.searchevent = "";
+        this.eventInfo = "";
       })
     },
+    search(){
+            if(this.searchevent !== ""){
+                axios.get(API_URL + "/search/" + this.searchevent).then(res => {
+                this.eventLists = res.data;
+                })
+            }else{
+                this.getEvent();
+            }
+            
+        },
 
     ShowProfile(profile){
       this.profile = profile
@@ -82,6 +98,18 @@ export default {
 </script>
 
 <style scoped>
+  input[type=search]{
+    border: 1px solid rgb(1, 112, 156);
+    outline: none;
+    margin-left: 67%;
+    border-radius: 10px;
+    padding:5px 10px;
+    margin-top: 2%;
+    background: rgb(202, 233, 248);
+    color: rgb(10, 5, 1);
+    text-align: center;
+    
+  }
   body{
     margin: 0;
     padding: 0;
@@ -106,9 +134,9 @@ export default {
     justify-content: space-evenly;
     flex-wrap: wrap;
     align-items: center;
-    width: 80%;
-    margin-left: 10%;
-    margin-top: 5%;
+    width: 70%;
+    margin-left: 15%;
+    margin-top: 2%;
   }
 </style>
 
