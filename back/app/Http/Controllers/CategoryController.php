307020080462de4e78edb26f7b5a36a089deca67
vantheav <vantheav.thav@student.passerellesnumeriques.org>
2021-12-13 07:51:25 +0700

    nothing

--- back/app/Http/Controllers/CategoryController.php
+++ back/app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
      */
     public function getCategory()
     {
-        return Category::latest()->get();
+        return Category::with('event')->latest()->get();
     }
 
     /**
